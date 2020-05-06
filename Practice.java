198cdfae220c53f085902fc8a7d52558ac840785
Riley <59904480+Riley-Volk@users.noreply.github.com>
2020-05-06 09:55:01 -0400

    Add files via upload
    UNFINISHED

---
+++ Practice.java
@@ -0,0 +1,11 @@
+/**
+ * Practicing Java and writing the hello world program.
+ */
+public class Practice {
+    /**
+     * Command line arguments.
+     */
+    public static void main(String args[]) {
+        System.out.print("Hello, World! ");
+    }
+}
